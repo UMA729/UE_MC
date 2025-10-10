fa727149f155ed251bc63704fb98abc9bfba1097
DESKTOP-DJOMAJ1\GA2B <h1202419009@hiratagakuen.onmicrosoft.com>
2025-10-10 13:54:44 +0900

    test

---
+++ MonsterCatcher/Source/MonsterCatcher/MonsterCatcher.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MonsterCatcher.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MonsterCatcher, "MonsterCatcher" );
